dfffaa0bc6cd2662c7bfeb336405a07779c13d58
T.Ruchitha <thumuruchi@gmail.com>
2026-09-20 11:25:53 +0530

    Add web scraping functionality with BeautifulSoup

---
+++ web_scraping.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://example.com"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+print("Page Title:")
+print(soup.title.text)
